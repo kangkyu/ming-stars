420a99d974ec4113d73f635a933cfdfb3a247d55
Kang-Kyu Lee <kangkyu1111@gmail.com>
2020-11-01 00:47:40 -0700

    Use puts for logs
    Fix: work with multiple names
    Add: form validation 'required' for text area

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,18 +2,26 @@ require 'csv'
 
 class AccountsController < ApplicationController
   def create
-    params[:names].split("\r\n").each do |account_name|
-      account_name = account_name.tr(' ', '')
-      uri = URI("https://www.instagram.com/#{account_name}/?__a=1")
-      res = Net::HTTP.get_response(uri)
-      body = JSON(res.body) if res.is_a?(Net::HTTPSuccess)
-
-      Tempfile.open do |tempfile|
-        CSV.open(tempfile.path, "wb", write_headers: true, headers: ['account_name', 'business_email', 'business']) do |csv|
-          csv << [account_name, body['graphql']['user']['business_email'], body['graphql']['user']['is_business_account']]
+    headers = ['account_name', 'business_email', 'business']
+    filename = "business emails #{Date.today}.csv"
+    Tempfile.open do |tempfile|
+      CSV.open(tempfile.path, "wb", write_headers: true, headers: headers) do |csv|
+        params[:names].split("\r\n").each do |account_name|
+          account_name = account_name.tr(' ', '')
+          uri = URI("https://www.instagram.com/#{account_name}/?__a=1")
+          res = Net::HTTP.get_response(uri)
+          if res.is_a?(Net::HTTPSuccess)
+            body = JSON(res.body)
+            business_email = body['graphql']['user']['business_email']
+            is_business = body['graphql']['user']['is_business_account']
+            csv << [account_name, business_email, is_business]
+          else
+            puts res
+            puts res.body
+          end
         end
-        send_data File.read(tempfile.path), filename: "business emails #{Date.today}.csv", type: 'text/csv'
       end
+      send_data File.read(tempfile.path), filename: filename, type: 'text/csv'
     end
   end
 end
